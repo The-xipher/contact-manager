2c55e402bf9db65ac61d0cc40b4664658d4443a6
Amith <amithmp676@gmail.com>
2024-08-16 10:50:56 +0530

    my code

--- src/main/java/com/ust/ContactRepository.java
+++ src/main/java/com/ust/ContactRepository.java
@@ -2,29 +2,46 @@ package com.ust;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ContactRepository {
 
     private List<Contact> contacts = new LinkedList();
-
+        long id=0;
     private long generateId() {
-        // TODO
+        return id++;
+
     }
 
     public Contact save(Contact contact) {
-        // TODO
+        contacts.add(contact);
+        return contact;
     }
 
-    public Contact findById(String id) throws ContactNotFoundException {
+    public List<Contact> findById(long id) throws ContactNotFoundException {
         // TODO
+        List<Contact> collect = contacts.stream()
+                .filter(contact -> contact.getId()==(id))
+                .collect(Collectors.toList());
+        return collect;
     }
 
     public List<Contact> findByPhoneNumber(String phoneNumber) throws ContactNotFoundException {
         // TODO
+
+        List<Contact> collect = contacts.stream()
+                .filter(contact -> contact.getPhoneNumbers().contains(phoneNumber))
+                .collect(Collectors.toList());
+        return collect;
     }
 
     public List<Contact> findByName(String name) throws ContactNotFoundException {
         // TODO
+
+        List<Contact> collect = contacts.stream()
+                .filter(contact -> contact.getName().equals(name))
+                .collect(Collectors.toList());
+        return collect;
     }
 
 }
